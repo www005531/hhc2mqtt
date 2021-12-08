f2b97963994420028fd82c48713f7c861ffa167c
Andrzej Łukasz Okoński <andrzej.lukasz.okonski@gmail.com>
2021-12-08 11:22:10 +0100

    first commit

---
+++ src/main/java/ao/hhc/HHCClient.java
@@ -0,0 +1,215 @@
+package ao.hhc;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class HHCClient {
+    public static final int READ_TIMEOUT = 10 * 1000;
+    public static final int LINGER_TIME = 100;
+    public static final int SEND_DELAY = 0;
+    public static final int RESPONSE_DELAY = 100;
+    public static final String IP = "192.168.102.12";
+    public static final int PORT = 5000;
+    //public static final String IP = "192.168.102.4";
+    //public static final int PORT = 4999;
+
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    String ip;
+    int port;
+    public static final String OLD_PREFIX = "B-";
+    public static final String PREFIX = "C-";
+    private static final Logger logger = LogManager.getLogger(HHCClient.class);
+
+    public HHCClient() throws Exception {
+        ip = IP;
+        port = PORT;
+        startConnection(ip, port);
+    }
+
+    public String getDeviceName() {
+
+        return PREFIX + ("HHC_" + IP + ":" + PORT).replaceAll("[^A-Za-z0-9]", "").toUpperCase();
+    }
+
+    public String getDeviceElementName(int dev, boolean isSwitch) {
+
+        return PREFIX + ("HHC_" + IP + ":" + PORT).replaceAll("[^A-Za-z0-9]", "").toUpperCase() + (isSwitch ? "-switch" : "-in") + dev;
+    }
+
+    public String getDeviceElementUUID(int dev, boolean isSwitch) {
+        return PREFIX + ("HHC_" + IP + ":" + PORT).replaceAll("[^A-Za-z0-9]", "").toUpperCase() + (isSwitch ? "-switch" : "-in") + dev;
+    }
+
+    private void startConnection(String ip, int port) throws Exception {
+        boolean tryConnect = true;
+        int retryCount = 0;
+        while (tryConnect) {
+            try {
+                clientSocket = new Socket(ip, port);
+                clientSocket.setSoLinger(true, LINGER_TIME);
+                clientSocket.setSoTimeout(READ_TIMEOUT);
+                out = new PrintWriter(clientSocket.getOutputStream(), false);
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                tryConnect = false;
+            } catch (Exception e) {
+                error(e.getMessage());
+                error("starting connection... retry count=" + (retryCount++) + " threadId=" + Thread.currentThread().getId());
+                Thread.sleep(2000);
+            }
+        }
+        debug("connection started for thread " + Thread.currentThread().getId() + ". redone " + retryCount + " times.");
+    }
+
+    static int lp = 0;
+
+    public synchronized String sendMessage(String msg) throws Exception {
+        return sendMessage(msg, false);
+    }
+
+    private String sendMessage(String msg, boolean debug) throws Exception {
+        //if(clientSocket==null || !clientSocket.isConnected() || clientSocket.isClosed()) startConnection(ip,port);
+        try {
+            Thread.sleep(SEND_DELAY);
+            out.print(msg);
+            out.flush();
+            String resp = readResponse();
+            if (debug) {
+                return (lp++) + " " + msg + "->" + resp + " " + clientSocket.hashCode();
+            } else {
+                return resp;
+            }
+        } catch (Exception e) {
+            error(e.getMessage());
+            stopConnection();
+            startConnection(ip, port);
+            return null;
+        }
+
+    }
+
+    private boolean respIsOK(String r) throws Exception {
+        boolean retOn = r.matches("on\\d{1}");
+        boolean retOff = r.matches("off\\d{1}");
+        boolean retInput = r.matches("input\\d{8}");
+        boolean retRead = r.matches("relay\\d{8}");
+        boolean retName = r.matches("name=\\\"(.*)\\\"");
+        boolean ret = retOn || retOff || retInput || retRead || retName;
+        debug("r=" + r + " ret=" + ret + " on=" + retOn + " off=" + retOff + " input=" + retInput + " switch=" + retRead +
+                " retName=" + retName +
+                " closed/out/in:" +
+                clientSocket.isClosed() + "/" + clientSocket.isOutputShutdown() + "/" + clientSocket.isInputShutdown());
+        return ret;
+    }
+
+    private String readResponse() throws Exception {
+        Thread.sleep(RESPONSE_DELAY);
+        String s = "";
+        int readCount = 0;
+        while (in.ready() || !respIsOK(s)) {
+            try {
+                s += "" + (char) in.read();
+                if (readCount++ > 20) throw new SocketException();
+            } catch (SocketException | SocketTimeoutException se) {
+                stopConnection();
+                startConnection(ip, port);
+                return null;
+            }
+            if (respIsOK(s)) break;
+            Thread.sleep(1); // prevent killing CPU
+        }
+        return s;
+    }
+
+
+    public void stopConnection() throws Exception {
+
+        try {
+            in.close();
+        } catch (Exception e) {
+            //e.printStackTrace();
+        }
+        try {
+            out.close();
+        } catch (Exception e) {
+            //e.printStackTrace();
+        }
+        try {
+            clientSocket.close();
+        } catch (Exception e) {
+            //e.printStackTrace();
+        }
+        debug("connection stopped");
+    }
+
+    public void testCommands() throws Exception {
+        HHCClient cc = new HHCClient();
+
+        while (Math.random() < 10) {
+            debug(cc.sendMessage("name"));
+            Thread.sleep(1000);
+        }
+        if (true) return;
+        //cc.startConnection("192.168.102.4",4999);
+        while (true) {
+            debug(Thread.currentThread().getId() + " " + cc.clientSocket.isConnected() + " " + cc.clientSocket.isInputShutdown() + " " + cc.clientSocket.isOutputShutdown());
+
+            if (Math.random() > 0.5) {
+                debug(Thread.currentThread().getId() + " " + cc.sendMessage("on1"));
+                debug(Thread.currentThread().getId() + " " + cc.sendMessage("on2"));
+                debug(Thread.currentThread().getId() + " " + cc.sendMessage("on3"));
+                debug(Thread.currentThread().getId() + " " + cc.sendMessage("on4"));
+                debug(Thread.currentThread().getId() + " " + cc.sendMessage("on5"));
+                debug(Thread.currentThread().getId() + " " + cc.sendMessage("on6"));
+                debug(Thread.currentThread().getId() + " " + cc.sendMessage("on7"));
+                debug(Thread.currentThread().getId() + " " + cc.sendMessage("on8"));
+                debug(Thread.currentThread().getId() + " " + cc.sendMessage("read"));
+                debug(Thread.currentThread().getId() + " " + cc.sendMessage("off1"));
+            } else {
+                debug(Thread.currentThread().getId() + " " + cc.sendMessage("off2"));
+                debug(Thread.currentThread().getId() + " " + cc.sendMessage("off3"));
+                debug(Thread.currentThread().getId() + " " + cc.sendMessage("off4"));
+                debug(Thread.currentThread().getId() + " " + cc.sendMessage("off5"));
+                debug(Thread.currentThread().getId() + " " + cc.sendMessage("off6"));
+                debug(Thread.currentThread().getId() + " " + cc.sendMessage("off7"));
+                debug(Thread.currentThread().getId() + " " + cc.sendMessage("off8"));
+                debug(Thread.currentThread().getId() + " " + cc.sendMessage("input"));
+                debug(Thread.currentThread().getId() + " " + cc.sendMessage("read"));
+            }
+        }
+    }
+
+    public static void debug(String s) {
+        logger.debug(s);
+    }
+    public static void error(String s) {
+        logger.error(s);
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        for (int i = 0; i < 1; i++) {
+            Thread t = new Thread(new Runnable() {
+
+                public void run() {
+                    try {
+                        new HHCClient().testCommands();
+                    } catch (Exception e) {
+                        error(e.getMessage());
+                    }
+                    ;
+                }
+            });
+            t.start();
+
+        }
+    }
+}
---
+++ src/main/java/ao/hhc/MQTTClient.java
@@ -0,0 +1,333 @@
+package ao.hhc;
+
+
+import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.UUID;
+
+public class MQTTClient {
+    public static final String SERVER_URI =  System.getProperty("SERVER_URI", "tcp://192.168.102.6:1883");
+    public static final String MQTT_SERVER_LOGIN = System.getProperty("MQTT_SERVER_LOGIN", "robo1");
+    public static final String MQTT_SERVER_PASSWORD =  System.getProperty("MQTT_SERVER_PASSWORD", "Qwas!100");
+    public static final  String BASE_TOPIC = System.getProperty("BASE_TOPIC", "homeassistant");
+    public static final String HA_DISCOVERY_TOPIC = System.getProperty("HA_DISCOVERY_TOPIC", "homeassistant");
+
+
+    IMqttClient client;
+    HHCClient cc = new HHCClient();
+
+    static {
+        // BASE_TOPIC = HA_DISCOVERY_TOPIC;
+
+        // BasicConfigurator.configure();
+    }
+
+    String lastInput = null;
+
+    private static final Logger logger = LogManager.getLogger(MQTTClient.class);
+
+    public MQTTClient() throws Exception {
+
+        String publisherId = "HHCClient_" + UUID.randomUUID().toString();
+
+        client = new MqttClient(SERVER_URI, publisherId,
+                new MqttDefaultFilePersistence(System.getProperty("TEMP") + "/MQTTCLIENT"));
+
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setUserName(MQTT_SERVER_LOGIN);
+        options.setPassword(MQTT_SERVER_PASSWORD.toCharArray());
+        options.setAutomaticReconnect(true);
+        options.setCleanSession(false); // czyli w razie rozłączenia przechowywana jest informacja o subskrypcjach
+        options.setConnectionTimeout(10);
+        options.setMaxInflight(100);
+
+        client.connect(options);
+    }
+
+    public void process() throws Exception {
+
+        //msg.setQos(0);
+        //msg.setRetained(true);
+
+        //  publisher.publish("hhc/sw/1/available",new MqttMessage("offline".getBytes()));
+
+
+//        publisher.publish("hhc/sw/1/state",new MqttMessage("ON".getBytes()));
+        discoverSwitches();
+
+        discoverBinaryInputs();
+
+
+        for (int dev = 1; dev < 9; dev++) {
+            cleanUpOldVersion(dev);
+
+        }
+
+
+//        client.publish(BASE_TOPIC+"/switch/" + cc.getDeviceName(1) + "/state", new MqttMessage("ON".getBytes()));
+// command //        client.publish("hhc/sw/1/set",new MqttMessage("OFF".getBytes()));
+
+    }
+
+
+    public void discoverBinaryInputs() throws Exception {
+        for (int dev = 1; dev < 9; dev++) {
+
+            MqttMessage msg = new MqttMessage(("{\"name\": \"" + cc.getDeviceElementName(dev, false) + "\", " +
+                    //   " \"force_update\": true, \"qos\": 1 , " +
+                    " \"unique_id\": \"" + cc.getDeviceElementUUID(dev, false) + "\", " +
+                    " \"availability\": [{ \"topic\": \""+BASE_TOPIC+"/binary_sensor/" + cc.getDeviceName() + "/input" + dev + "/availability\", " +
+                    " \"payload_available\": \"online\", \"payload_not_available\": \"offline\" } ] , " +
+                    // " \"qos\": \"0\", " +
+                    //    " \"device_class\": \"motion\", " +
+
+                    " \"device\":{\"identifiers\":[\"" + cc.getDeviceName() + "\"]," +
+                    " \"manufacturer\":\"CHRL\",\"model\":\"HHC-N8180P\",\"name\":\"HHC" + "\",\"sw_version\":\"" + HHCClient.PREFIX + "1.1\", " +
+                    " \"suggested_area\": \"AUTOMATYKA\"}, " +
+                    // " \"command_topic\": \""+BASE_TOPIC+"/switch/" + cc.getDeviceName(dev) + "/set\", " +
+                    " \"state_topic\": \""+BASE_TOPIC+"/binary_sensor/" + cc.getDeviceName() + "/input" + dev + "/state\" } ").getBytes()
+            );
+            msg.setRetained(true);
+            client.publish(HA_DISCOVERY_TOPIC+"/binary_sensor/" + cc.getDeviceName() + "/input" + dev + "/config",
+                    msg);
+            //subscribe(dev);
+        }
+        for (int dev = 1; dev < 9; dev++) {
+
+            //subscribe(dev);
+        }
+        for (int dev = 1; dev < 9; dev++) {
+            setBinaryInputsAvailable(dev);
+        }
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+
+                while (true) {
+                    try {
+                        //Thread.sleep(100);
+                        String in = cc.sendMessage("input");
+                        if (in == null) in = "";
+                        in = in.trim();
+                        if (in.equals(lastInput)) continue;
+                        lastInput = in;
+                        char a[] = in.toCharArray();
+                        debug("input: " + in);
+                        // "input00000001";
+                        if (in.indexOf("input") > -1) {
+                            for (int dev = 1; dev < 9; dev++) {
+                                boolean one = ("" + a[9 - dev + 4]).equals("1");
+                                debug("dev[" + dev + "]=" + (one ? "1" : "0"));
+                                client.publish(BASE_TOPIC+ "/binary_sensor/" + cc.getDeviceName() + "/input" + dev + "/state",
+                                        new MqttMessage((one ? "ON" : "OFF").getBytes()));
+                                // setBinaryInputsAvailable(dev)
+                            }
+
+                        }
+
+
+                    } catch (Exception e) {
+                        error(e.getMessage());
+                        try {
+                            Thread.sleep(1000);
+                        } catch (Exception e2) {
+                            error(e2.getMessage());
+                        }
+                    }
+
+                }
+            }
+        }).start();
+
+    }
+
+
+    ////////////////////////
+
+
+    public void discoverSwitches() throws Exception {
+        for (int dev = 1; dev < 9; dev++) {
+
+            MqttMessage msg = new MqttMessage(("{\"name\": \"" + cc.getDeviceElementName(dev, true) + "\", " +
+                    //   " \"force_update\": true, \"qos\": 1 , " +
+                    " \"unique_id\": \"" + cc.getDeviceElementUUID(dev, true) + "\", " +
+                    " \"availability\": [{ \"topic\": \""+BASE_TOPIC+"/switch/" + cc.getDeviceName() + "/switch" + dev + "/availability\", " +
+                    " \"payload_available\": \"online\", \"payload_not_available\": \"offline\" } ] , " +
+                    // " \"qos\": \"0\", " +
+                    //   " \"device_class\": \"power\", " +
+
+                    " \"device\":{\"identifiers\":[\"" + cc.getDeviceName() + "\"]," +
+                    " \"manufacturer\":\"CHRL\",\"model\":\"HHC-N8180P\",\"name\":\"HHC" + "\",\"sw_version\":\"" + HHCClient.PREFIX + "1.1\", " +
+                    " \"suggested_area\": \"AUTOMATYKA\"}, " +
+                    " \"command_topic\": \""+BASE_TOPIC+"/switch/" + cc.getDeviceName() + "/switch" + dev + "/set\", " +
+                    " \"state_topic\": \""+BASE_TOPIC+ "/switch/" + cc.getDeviceName() + "/switch" + dev + "/state\" } ").getBytes()
+            );
+            msg.setRetained(true);
+            String s2 = HA_DISCOVERY_TOPIC+ "/switch/" + cc.getDeviceName() + "/switch" + dev + "/config";
+            client.publish(s2,
+                    msg);
+            logger.debug("s2="+s2+" msg="+new String(msg.getPayload()));
+            //MqttTopic topic = client.getTopic(s2);
+            //subscribe(dev);
+            //logger.debug("topic="+topic);
+        }
+        for (int dev = 1; dev < 9; dev++) {
+
+            subscribeSwitch(dev);
+        }
+        readSwitches();
+
+        for (int dev = 1; dev < 9; dev++) {
+            setSwitchesAvailable(dev);
+        }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                while (true) {
+                    try {
+                        if (!client.isConnected()) {
+                            debug("MQTT Client is disconnected");
+                            Thread.sleep(1000);
+                            continue;
+                        }
+                        for (int dev = 1; dev < 9; dev++) {
+                            setSwitchesAvailable(dev);
+                            setBinaryInputsAvailable(dev);
+                        }
+                        Thread.sleep(60 * 1000);
+                    } catch (Exception e) {
+                        error("MONITOR: " + e.getMessage());
+
+                    }
+                }
+            }
+        }).start();
+
+    }
+
+
+    public void subscribeSwitch(int dev) throws Exception {
+        debug("subscribe " + dev);
+        client.subscribe(BASE_TOPIC+"/switch/" + cc.getDeviceName() + "/switch" + dev + "/set", (topic, msg) -> {
+            try {
+                debug("<subscribe>");
+                byte[] payload = msg.getPayload();
+                debug(new String(payload) + "=");
+
+                String devCmd = new String(payload);
+                if (devCmd.toUpperCase().indexOf("ON") > -1) {
+                    devCmd = "on" + dev;
+                } else {
+                    devCmd = "off" + dev;
+                }
+                String ret = cc.sendMessage(devCmd);
+                debug("ret=[" + ret + "]");
+
+//                int i = Integer.parseInt("" + new String(payload).replaceAll("on", "").replaceAll("off", ""));
+                String stateTopic = BASE_TOPIC+"/switch/" + cc.getDeviceName() + "/switch" + dev + "/state";
+                debug("sending on topic=" + topic + " stateTopic=" + stateTopic + " message: " + msg);
+                client.publish(stateTopic, new MqttMessage(payload));
+                debug("Message sent.");
+            } catch (Exception e) {
+                error(e.getMessage());
+            }
+        });
+    }
+
+
+    public void setBinaryInputsAvailable(int dev) throws Exception {
+        setAvailable(dev, true, true);
+    }
+
+    public void setSwitchesAvailable(int dev) throws Exception {
+        setAvailable(dev, true, false);
+    }
+
+    private void setAvailable(int dev, boolean available, boolean isBinaryInput) throws Exception {
+        debug("sending available dev:" + dev + " available=" + available);
+        String topic = BASE_TOPIC+"/switch/" + cc.getDeviceName() + "/switch" + dev + "/availability";
+
+        if (isBinaryInput)
+            topic = BASE_TOPIC+"/binary_sensor/" + cc.getDeviceName() + "/input" + dev + "/availability";
+        client.publish(topic, new MqttMessage((available ? "online" : "offline").getBytes()));
+    }
+
+    public void cleanUpOldVersion(int dev) throws Exception {
+
+        String removeTopic = HA_DISCOVERY_TOPIC+"/switch/" + cc.getDeviceElementName(dev, true).replaceAll(HHCClient.PREFIX, HHCClient.OLD_PREFIX).
+                replaceAll("5000", "4999").replaceAll("10212", "1024") + "/switch/config";
+        debug("removeTopic=" + removeTopic);
+        client.publish(removeTopic, new byte[0], 0, true);
+        removeTopic = HA_DISCOVERY_TOPIC+"/switch/" + cc.getDeviceElementName(dev, true).replaceAll(HHCClient.PREFIX, HHCClient.OLD_PREFIX)
+                + "/switch/config";
+        debug("removeTopic=" + removeTopic);
+        client.publish(removeTopic, new byte[0], 0, true);
+
+        removeTopic = BASE_TOPIC+"/switch/" + cc.getDeviceElementName(dev, true).replaceAll(HHCClient.PREFIX, HHCClient.OLD_PREFIX).
+                replaceAll("5000", "4999").replaceAll("10212", "1024") + "/switch";
+        debug("removeTopic=" + removeTopic);
+        client.publish(removeTopic, new byte[0], 0, true);
+
+        removeTopic = BASE_TOPIC+"/switch/" + cc.getDeviceElementName(dev, true).replaceAll(HHCClient.PREFIX, HHCClient.OLD_PREFIX) + "/switch";
+        debug("removeTopic=" + removeTopic);
+        client.publish(removeTopic, new byte[0], 0, true);
+
+
+        removeTopic = BASE_TOPIC+"/switch/" + cc.getDeviceElementName(dev, true).replaceAll(HHCClient.PREFIX, HHCClient.OLD_PREFIX).
+                replaceAll("5000", "4999").replaceAll("10212", "1024") + "/";
+        debug("removeTopic=" + removeTopic);
+        client.publish(removeTopic, new byte[0], 0, true);
+
+        removeTopic = BASE_TOPIC+"/switch/" + cc.getDeviceElementName(dev, true).replaceAll(HHCClient.PREFIX, HHCClient.OLD_PREFIX) + "/";
+        debug("removeTopic=" + removeTopic);
+        client.publish(removeTopic, new byte[0], 0, true);
+    }
+
+
+    public void readSwitches() throws Exception {
+        String in = cc.sendMessage("read");
+        if (in == null) in = "";
+        in = in.trim();
+        char a[] = in.toCharArray();
+        debug("relay: " + in);
+        // "input00000001";
+        if (in.indexOf("relay") > -1) {
+            for (int dev = 1; dev < 9; dev++) {
+                boolean one = ("" + a[9 - dev + 4]).equals("1");
+                debug("dev[" + dev + "]=" + (one ? "1" : "0"));
+                client.publish(BASE_TOPIC+"/switch/" + cc.getDeviceName() + "/switch" + dev + "/state",
+                        new MqttMessage((one ? "ON" : "OFF").getBytes()));
+                //setSwitchesAvailable(dev);
+            }
+        }
+    }
+
+    public static void debug(String s) {
+        logger.debug(s);
+    }
+
+    public static void error(String e) {
+        logger.error(e);
+    }
+
+    public static void main(String[] args) throws Exception {
+
+
+        MQTTClient c = new MQTTClient();
+        c.process();
+        // c.cc.sendMessage("off1");
+        // c.cc.sendMessage("off3");
+
+        // c.readSwitches();
+
+
+    }
+}
